e83715675404f33e933b24b34fa1a690f3fa7d98
mohamed alaa <mozamezo638@gmail.com>
2022-09-25 10:45:57 +0200

    commit	new file:   accounts/__init__.py commit	new file:   accounts/__pycache__/__init__.cpython-39.pyc commit	new file:   accounts/__pycache__/admin.cpython-39.pyc commit	new file:   accounts/__pycache__/apps.cpython-39.pyc commit	new file:   accounts/__pycache__/models.cpython-39.pyc commit	new file:   accounts/__pycache__/urls.cpython-39.pyc commit	new file:   accounts/__pycache__/views.cpython-39.pyc commit	new file:   accounts/admin.py commit	new file:   accounts/apps.py commit	new file:   accounts/migrations/__init__.py commit	new file:   accounts/migrations/__pycache__/__init__.cpython-39.pyc commit	new file:   accounts/models.py commit	new file:   accounts/tests.py commit	new file:   accounts/urls.py commit	new file:   accounts/views.py commit	modified:   blog/__pycache__/views.cpython-39.pyc commit	modified:   blog/views.py commit	modified:   blog_project/__pycache__/settings.cpython-39.pyc commit	modified:   blog_project/__pycache__/urls.cpython-39.pyc commit	modified:   blog_project/settings.py commit	modified:   blog_project/urls.py commit	modified:   db.sqlite3 commit	modified:   templates/base.html commit	new file:   templates/registration/login.html commit	new file:   templates/signup.html

--- blog/views.py
+++ blog/views.py
@@ -29,3 +29,5 @@ class BlogDeleteView(DeleteView):
   model = Post
   template_name ='post_delete.html'
   success_url = reverse_lazy('home')
+
+
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +127,6 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
